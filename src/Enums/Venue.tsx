9b561c8ad7d32ec07d7f0da5d217ff8b9607e55b
TToth <toth.kristof.laszlo@gmail.com>
2024-07-26 18:37:53 +0200

    styling

---
+++ src/Enums/Venue.tsx
@@ -0,0 +1,25 @@
+function Venues(m: string): string {
+	const keys = new Map<string, string>();
+	keys.set("a38", "src/assets/VenueIcons/a38.svg");
+	keys.set("akvariumklub", "src/assets/VenueIcons/akvarium.svg");
+	keys.set("aurora", "src/assets/VenueIcons/aurora.png");
+	keys.set("barbanegrar", "src/assets/VenueIcons/barbanegraredstage.png");
+	keys.set("barbanegrab", "src/assets/VenueIcons/barbanegrabluestage.png");
+	keys.set("durerkert", "src/assets/VenueIcons/durerkert.svg");
+	keys.set("mvmdome", "src/assets/VenueIcons/mvmdome.png");
+	keys.set("riff", "src/assets/VenueIcons/riff.png");
+	
+	function getVenue(m: string): string {
+		for (const mKey of keys) {
+			if(mKey[0].includes(m)) {
+				return mKey[1] as string;
+			}
+		}
+		
+		return "";
+	}
+	
+	return getVenue(m);
+}
+
+export default Venues;
--- src/EventCard/EventCard.tsx
+++ src/EventCard/EventCard.tsx
@@ -1,13 +1,14 @@
 import {v4 as uuidv4} from 'uuid';
 import {Event, Genre} from '../Interfaces/Interfaces.tsx';
 import './EventCard.scss';
+import venue from "../Enums/Venue.tsx";
 
 function EventCard({ EventObject, Genres} : {EventObject: Event, Genres: Array<Genre>}) {
 	const Name: string = EventObject.name;
 	const EventDate: Date = new Date(EventObject.date);
 	const EventTime: string | undefined = EventObject.time //new Date(1970, 0, 1, EventObject.Time?.getHours(), EventObject.Time?.getMinutes(), EventObject.Time?.getSeconds());
 	const Room: string | undefined = EventObject.room;
-	const Description: string | undefined = EventObject.description;
+	//const Description: string | undefined = EventObject.description;
 	const URL: string = EventObject.url;
 	const CoverURL: string | undefined = EventObject.coverUrl;
 	
@@ -22,14 +23,16 @@ function EventCard({ EventObject, Genres} : {EventObject: Event, Genres: Array<G
 					<a className={"eventName"} href={URL} target={"_blank"}>{Name}</a>
 					<p className={"eventDate"}>{(!EventTime) ? EventDate.toLocaleDateString("hu-HU") : Date.parse(EventDate + "T" + EventTime)}</p>
 				</div>
-				<div className={"eventDescription"}>
-					<p>{Description}</p>
-				</div>
 				<div className={"eventMisc"}>
 					<div className={"eventGenres"}>
-						{Genres.map((g: Genre ) => (<span key={uuidv4()}>{g.genre1}</span>))}
+						{Genres.map((g: Genre ) => (<span className={"eventGenre"} key={uuidv4()}>{g.genre1}</span>))}
 					</div>
-					{!(!Room) && <div className={"eventRoom"}></div>}
+					<div className={"eventVenueInfo"}>
+						<div className={"eventVenue"}>
+							<img className={"venue"} src={venue("a38")} alt={"venue"}/>
+						</div>
+						{!(!Room) && <div className={"eventRoom"}>{Room}</div>}
+					</div>
 				</div>
 			</div>
 		</div>
